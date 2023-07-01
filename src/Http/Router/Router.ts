c136c8a6a7178db813ac794474b0507ce23ca99d
Matheus <matheus.bickel@bagy.com.br>
2023-07-01 18:09:24 -0300

    task: adding an abstract HttpClient implementation based on the Express framework to generate decoupling.

---
+++ src/Http/Router/Router.ts
@@ -0,0 +1,12 @@
+import { HttpClient } from "../HttpClient/HttpClient";
+
+export class Router {
+    constructor(protected HttpClient: HttpClient){}
+
+    async init(): Promise<void> 
+    {
+        this.HttpClient.on('get', '/',async (params:string, body: unknown) => {
+            return "TESTE"
+        })
+    }
+}
