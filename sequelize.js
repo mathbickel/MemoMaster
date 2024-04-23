2f35417574a311d3aa948836bc1db63d4bdddde5
Matheus <matheus.bickel@bagy.com.br>
2024-04-23 12:35:38 -0300

    task: adiciona sequelize para interação com banco de dados postgres

---
+++ sequelize.js
@@ -0,0 +1,8 @@
+module.exports = {
+    username: 'root',
+    password: 'root',
+    database: 'memomaster',
+    host: '127.0.0.1',
+    dialect: 'postgres',
+    port: '5432'
+}
--- src/Domains/MemoData/Infra/DataRepositoryPostgres.ts
+++ src/Domains/MemoData/Infra/DataRepositoryPostgres.ts
@@ -1,53 +1,28 @@
-import postgres from "postgres";
 import { inject, injectable } from "tsyringe";
-import { DbCommand } from "../../../common/database/DbCommand";
 import { DataRepository } from "../Domain/DataRepository";
 import { IData } from "../Domain/IData";
-import { STORE } from "./sql/store";
 
 @injectable()
 export class DataRepositoryPostgres implements DataRepository {
-    constructor(@inject('DbConnection') private conn: postgres.Sql){}
+    constructor(@inject('DbConnection') private conn){}
 
     async store(data: IData[]): Promise<void> {
-        try {
-            
-        } finally {
-            this.conn.CLOSE
-        }    
+        
     }
 
-    getAll(): Promise<IData[]> {
-        throw new Error("Method not implemented.");
-    }
-    getById(id: number): Promise<IData> {
-        throw new Error("Method not implemented.");
-    }
-    update(id: number): Promise<IData> {
-        throw new Error("Method not implemented.");
-    }
-    remove(id: number): Promise<IData> {
+    async getAll(): Promise<IData[]> {
         throw new Error("Method not implemented.");
     }
 
-    private async getStored(data: IData[], command: DbCommand<any, any>):Promise<void> {
-        data.map(async (data: IData) => {
-            await command.execute({
-                commandText: STORE,
-                binds: [
-                    data.id,
-                    data.title,
-                    data.author,
-                    data.category,
-                    data.content,
-                    data.description,,
-                    data.tags,
-                    data.relatedlinks,
-                    data.source,
-                    data.updated_at,
-                    data.created_at
-                ]
-            })
-        })
+    async getById(id: number): Promise<IData> {
+        throw new Error("Method not implemented.");
+    }
+    
+    async update(id: number): Promise<IData> {
+        throw new Error("Method not implemented.");
+    }
+    
+    async remove(id: number): Promise<IData> {
+        throw new Error("Method not implemented.");
     }
 }
