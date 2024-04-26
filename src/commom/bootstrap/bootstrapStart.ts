5e01cde2a32e06634312f87a49fc01150b06b490
Matheus <matheus.bickel@bagy.com.br>
2024-04-26 16:03:51 -0300

    task: add bootstrap

---
+++ src/commom/bootstrap/bootstrapStart.ts
@@ -0,0 +1,3 @@
+export async function bootstrapStart(): Promise<void> {
+    
+}
--- src/commom/db/DbConnection.ts
+++ src/commom/db/DbConnection.ts
@@ -2,7 +2,7 @@ import { IData } from "../../Domains/MemoData/Domain/IData"
 import { queryResult } from "./types"
 
 export interface DbConnection {
-    command<T>(query: string, data?: IData[]): Promise<queryResult>
+    command(query: string, data?: IData[]): Promise<queryResult>
     open():Promise<void>
     close():Promise<void>
 }
